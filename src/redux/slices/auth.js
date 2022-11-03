e5eb351fba6da405fea83eea59b5da534cc5adb2
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-03 23:33:48 +0200

    Update auth.js

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -9,6 +9,22 @@ export const fetchAuth = createAsyncThunk(
   }
 );
 
+export const fetchRegister = createAsyncThunk(
+  "auth/fetchRegister",
+  async (params) => {
+    const { data } = await axios.post("/auth/register", params);
+    return data;
+  }
+);
+
+export const fetchAuthCurrent = createAsyncThunk(
+  "auth/fetchAuthCurrent",
+  async () => {
+    const { data } = await axios.get("/auth/current");
+    return data;
+  }
+);
+
 const initialState = {
   data: null,
   status: "loading",
@@ -17,6 +33,11 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
   extraReducers: {
     [fetchAuth.pending]: (state) => {
       state.status = "loading";
@@ -31,7 +52,37 @@ const authSlice = createSlice({
       state.status = "error";
       state.data = null;
     },
+    [fetchAuthCurrent.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+
+    [fetchAuthCurrent.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchAuthCurrent.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
+    [fetchRegister.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+
+    [fetchRegister.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchRegister.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
   },
 });
 
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
 export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
