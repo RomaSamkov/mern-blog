9270ce5c3a16045009a041fb2efaedd5245c9bb5
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-05 15:17:30 +0200

    Update posts.js

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -11,6 +11,14 @@ export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
   return data;
 });
 
+export const fetchRemovePost = createAsyncThunk(
+  "posts/fetchRemovePost",
+  async (id) => {
+    const { data } = await axios.delete(`/posts/${id}`);
+    return data;
+  }
+);
+
 const initialState = {
   posts: {
     items: [],
@@ -31,7 +39,6 @@ const postsSlice = createSlice({
       state.posts.items = [];
       state.posts.status = "loading";
     },
-
     [fetchPosts.fulfilled]: (state, action) => {
       state.posts.items = action.payload;
       state.posts.status = "loaded";
@@ -40,11 +47,11 @@ const postsSlice = createSlice({
       state.posts.items = [];
       state.posts.status = "error";
     },
+
     [fetchTags.pending]: (state) => {
       state.tags.items = [];
       state.tags.status = "loading";
     },
-
     [fetchTags.fulfilled]: (state, action) => {
       state.tags.items = action.payload;
       state.tags.status = "loaded";
@@ -52,6 +59,12 @@ const postsSlice = createSlice({
     [fetchTags.rejected]: (state) => {
       state.tags.items = [];
       state.tags.status = "error";
+    },
+
+    [fetchRemovePost.pending]: (state, action) => {
+      state.posts.items = state.posts.items.filter(
+        (obj) => obj._id !== action.meta.arg
+      );
     },
   },
 });
